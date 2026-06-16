eedb163ed75ed34c0981de0379b6c6d9e48237f3
puvithk <puvithkumar2004@gmail.com>
2026-06-16 14:35:31 +0530

    Corrected prompt

--- Day 37 Agent project/deep_research_agent/agent/question_decomposer.py
+++ Day 37 Agent project/deep_research_agent/agent/question_decomposer.py
@@ -2,6 +2,7 @@ from utils.llm import llm
 from typing import Annotated ,TypedDict
 from pydantic import Field , BaseModel
 from graph.state import State
+from langchain_core.messages import AIMessage  , HumanMessage
 
 class QuestionDecomposer(BaseModel):
     questions: list[str] = Field(
@@ -17,11 +18,29 @@ def question_decomposer(state : State) -> State:
     structured_output  = llm.with_structured_output(QuestionDecomposer)
 
     result = structured_output.invoke(f"""
-        User query 
-                                      {state['user_query']}    
-        Break this query into multiple research questions.    
+        Your are a deep researching agent  Break this query into multiple research questions.
+        User query :{state['user_query']}       
+        Generate all sub-questions required to perform deep research on this topic.
+
+        Cover:
+        - Definitions
+        - Concepts
+        - Comparisons
+        - Advantages
+        - Disadvantages
+        - Challenges
+        - Future scope
+
+    Return only the structured output.
         """)
     
     return {
-        "sub_questions" : result.model_dump()["questions"]
+        "sub_questions" : result.model_dump()["questions"],
+        "messages": [
+    HumanMessage(state['user_query'])
+    ,
+        AIMessage(
+            content="\n".join( result.model_dump()["questions"])
+        )
+    ]
     }
