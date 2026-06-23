e6783001f232493876f60cc225373319a488d78a
puvithk <puvithkumar2004@gmail.com>
2026-06-23 19:35:55 +0530

    Created a flow for agent

--- Day 38 Startup Idea Validator/startup_idea_validator/main_graph/graph.py
+++ Day 38 Startup Idea Validator/startup_idea_validator/main_graph/graph.py
@@ -1,1 +1,31 @@
 # Defination of the main graph 
+
+#Import the required libaries 
+
+from langgraph.graph import StateGraph , START , END 
+from .state import MainState
+
+
+# Build a basic agent 
+
+# Add the state 
+
+# Create node of each agent or operations 
+    # Question summarizer and decompose 
+    # Users approver 
+
+    # Compitator agent 
+
+    # Market agent 
+
+    # Customer agent 
+
+    # trend agent 
+
+
+# Create egde and conditioanl edge 
+
+
+# Complile the build and add memory 
+
+
