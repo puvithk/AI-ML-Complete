507c40c8232f3ac32faa09e5d8e64a454f306507
puvithk <puvithkumar2004@gmail.com>
2026-06-27 20:51:15 +0530

    Created web scraping tool

--- Day 38 Startup Idea Validator/startup_idea_validator/tools/web_scraper.py
+++ Day 38 Startup Idea Validator/startup_idea_validator/tools/web_scraper.py
@@ -1,9 +1,13 @@
-from langchain_community.tools import TavilySearchResults
+from langchain_community.tools import TavilySearchResults 
+from langchain_tavily.tavily_crawl import TavilyCrawl
 from langchain_core.tools import tool
 from dotenv import load_dotenv
 
 load_dotenv()
+
 tavily_search = TavilySearchResults(max_length =10)
+
+tavily_crawl = TavilyCrawl(max_depth=1, max_breadth=10, limit=10)
 
 @tool
 def web_search_tool(query :str , seen_urls :list=[])-> list:
@@ -21,3 +25,29 @@ def web_search_tool(query :str , seen_urls :list=[])-> list:
     # Return the result 
     return results
 
+
+@tool
+def web_scraping_tool(instructions : str , url:str = None):
+    """Web scraping tool used to get the details from the webpage based on the instruction and the urls
+    """
+
+    tavily_crawl.instructions =  instructions
+
+
+    response = tavily_crawl.invoke({
+        'url' : url
+    })
+
+    if response:
+        response['result'] = [ {"url" : i['url'] , "raw_content" : i['raw_content']} for i in response['results']]
+
+    return response
+
+
+if __name__ =='__main__':
+    response = web_scraping_tool.invoke({
+    "instructions": "Find merits of langchain",
+    "url": "https://docs.langchain.com/oss/python/build-overview"
+})
+    print(response)
+    print(type(response))
